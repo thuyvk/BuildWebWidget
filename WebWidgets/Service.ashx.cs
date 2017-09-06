315d3fa6d3423ee475f181dac10cd9fc83e22c98
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-09-06 14:03:27 +0700

    begin

---
+++ WebWidgets/Service.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebWidgets
+{
+    /// <summary>
+    /// Summary description for Service
+    /// </summary>
+    public class Service : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("<h1>Hello From Service</h1>");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
